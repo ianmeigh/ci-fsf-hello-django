b7646bbdf58019d076d3b035f3a697a770b28b7a
ianmeigh <ianmeigh@googlemail.com>
2022-06-14 14:15:54 +0100

    feat: create todo item model and register in admin.py to expose model to the admin portal

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "done")
+    readonly_fields = ("id",)
+
 
 # Register your models here.
+admin.site.register(Item, ItemAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # Override the inherited '__str__' method to return the Item 'name'
+    # def __str__(self):
+    #     return self.name
